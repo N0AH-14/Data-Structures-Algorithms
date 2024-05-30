5a281f1126d3d0220e4061d2d9b04597fac52f18
Krishna Jodha <Krishnajodha0147@gmail.com>
2024-05-30 23:57:15 +0530

    LC:215 Kth Largest Element

---
+++ Problems/Array/KthLargestElement.cpp
@@ -0,0 +1,26 @@
+int speedUP=[](){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        if(nums.size()==1) return nums[0];
+        int i;
+        multiset <int> kLargest;
+        for(i=0;i<k;++i)
+        {
+            kLargest.insert(nums[i]);
+        }
+        for(i=k;i<nums.size();++i)
+        {
+            if(nums[i]>*(kLargest.begin()))
+            {
+                kLargest.insert(nums[i]);
+                kLargest.erase(kLargest.begin());
+            }
+        }
+        return *(kLargest.begin());
+    }
+};
